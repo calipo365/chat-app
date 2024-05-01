c29bcb409a7d7232b2a7fbda45c479d13b5d9d28
calipo365 <nina.c.day@gmail.com>
2024-05-01 13:24:28 +1200

    Users working -user in progress

--- lab_4/chat-app-npx/src/components/User.tsx
+++ lab_4/chat-app-npx/src/components/User.tsx
@@ -1,5 +1,32 @@
+import axios from 'axios';
+import React from 'react';
+import {Link, useNavigate, useParams} from 'react-router-dom';
+
 const User = () => {
-    return (<h1>User</h1>)
+    
+    const {id} = useParams();
+    const navigate = useNavigate();
+    const [user, setUser] = React.useState<User>({user_id:0, username:""})
+    const [errorFlag, setErrorFlag] = React.useState(false)
+    const [errorMessage, setErrorMessage] = React.useState("")
+
+    React.useEffect(() => {
+        const getUser = () => {
+            axios.get('http://localhost:3000/api/users/' + id)
+                .then((response) => {
+                    setErrorFlag(false)
+                    setErrorMessage("")
+                    setUser(response.data)
+                }, (error) => {
+                    setErrorFlag(true)
+                    setErrorMessage(error.toString())
+                })
+        }
+        getUser()
+    }, [id])
+
+
+
 }
 
 export default User;
