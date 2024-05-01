ef86a0cfdbd0171a096790ee67cd6863b7c5a518
calipo365 <nina.c.day@gmail.com>
2024-05-01 14:32:55 +1200

    User, users and delete modal working

--- lab_4/chat-app-npx/src/components/User.tsx
+++ lab_4/chat-app-npx/src/components/User.tsx
@@ -25,7 +25,63 @@ const User = () => {
         getUser()
     }, [id])
 
+    const deleteUser = (user: User) => {
+        axios.delete('http://localhost:3000/api/users/' + user.user_id)
+            .then((response) => {
+                navigate('/users')
+            }, (error) => {
+                setErrorFlag(true)
+                setErrorMessage(error.toString())
+            })
+    }
 
+    if (errorFlag) {
+        return (
+            <div>
+                <h1>User</h1>
+                <div style={{ color:"red" }}>
+                    {errorMessage}
+                </div>
+                <Link to={"/users"}>Back to users</Link>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>User</h1>
+                {user.user_id}: {user.username}
+                <Link to={"/users"}>Back to users</Link>
+                <button type="button">
+                    Edit
+                </button>
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#deleteUserModal">
+                    Delete
+                </button>
+                    <div className="modal fade" id="deleteUserModal" tabIndex={-1} role="dialog"
+                        aria-labelledby="deleteUserModalLabel" aria-hiddden="true">
+                            <div className="modal-dialog" role="document">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id ="deleteUserModalLabel">Delete User</h5>
+                                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-dismiss="modal">
+                                            Close
+                                        </button>
+                                        <button type="button" className="btn btn-primary" data-dismiss="modal"
+                                                onClick={() => deleteUser(user)}>
+                                                    Delete User
+                                                </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+            </div>
+        )
+    }
 
 }
 
